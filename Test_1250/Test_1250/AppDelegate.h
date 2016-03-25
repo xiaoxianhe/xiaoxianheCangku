d8718d0940582ba2ad5fec322f262bf1671b09d2
lanou3g <843977657@qq.com>
2016-03-25 10:40:57 +0800

    new

---
+++ Test_1250/Test_1250/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_1250
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 TSN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
